d56f0edc7ff9351888a3b6cfe8d96d6d8f60bb4a
Muser Yeung <museryeung@banamedia.com>
2026-09-14 23:26:19 +0800

    Allow profile updates and avatar uploads during onboarding.

---
+++ nhost/migrations/default/20260914110000_avatars_bucket/up.sql
@@ -0,0 +1,20 @@
+INSERT INTO storage.buckets (
+  id,
+  download_expiration,
+  min_upload_file_size,
+  max_upload_file_size,
+  cache_control,
+  presigned_urls_enabled
+)
+VALUES (
+  'avatars',
+  604800,
+  1,
+  5242880,
+  'public, max-age=3600',
+  true
+)
+ON CONFLICT (id) DO UPDATE SET
+  max_upload_file_size = EXCLUDED.max_upload_file_size,
+  cache_control = EXCLUDED.cache_control,
+  presigned_urls_enabled = EXCLUDED.presigned_urls_enabled;
